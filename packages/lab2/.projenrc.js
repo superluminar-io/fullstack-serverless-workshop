2a882b57c48c78c5edb3d8c932a7b1ec9d12377a
Janosch Offenberg <janosch.offenberg@superluminar.io>
2023-03-31 13:33:33 +0200

    further move from lab5 to lab2

--- packages/lab2/.projenrc.js
+++ packages/lab2/.projenrc.js
@@ -2,7 +2,7 @@ const { awscdk, javascript } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.35.0',
   defaultReleaseBranch: 'main',
-  name: 'lab5',
+  name: 'lab2',
   github: false,
   packageManager: javascript.NodePackageManager.NPM,
   deps: [
@@ -16,10 +16,5 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   ],
 });
 
-project.addTask('test:e2e', {
-  exec: 'jest --testMatch "**/*.e2etest.ts"',
-});
 // Windows users might need this
-project.jest.addTestMatch('**/?(*.)+(spec|test).ts?(x)');
-project.jest.addIgnorePattern('.*/frontend/.*\\\.test\\\.[tj]sx?');
 project.synth();
