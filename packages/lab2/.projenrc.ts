85356468b9c0af56f47188167006ada3223f55f5
VerenaTraub <verenatraubmail@gmail.com>
2023-05-05 19:57:49 +0200

    update lab2 package

---
+++ packages/lab2/.projenrc.ts
@@ -0,0 +1,20 @@
+import { awscdk, javascript } from "projen";
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  github: false,
+  name: "notes-api",
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  deps: [
+    "@aws-sdk/client-dynamodb",
+    "@aws-sdk/lib-dynamodb",
+    "@aws-cdk/aws-apigatewayv2-alpha",
+    "@aws-cdk/aws-apigatewayv2-integrations-alpha",
+    "aws-sdk",
+    "fs-extra",
+  ],
+  devDeps: ["@types/fs-extra", "@types/aws-lambda"],
+});
+
+project.synth();
---
+++ packages/lab2/src/http-api.ts
@@ -0,0 +1,54 @@
+import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { CfnOutput } from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+
+export class HttpApi extends Construct {
+  public notesTable: dynamodb.Table;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.notesTable = new dynamodb.Table(this, "notes-table", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      stream: dynamodb.StreamViewType.NEW_IMAGE,
+    });
+
+    const putNote = new NodejsFunction(this, "put-note", {
+      environment: {
+        TABLE_NAME: this.notesTable.tableName,
+      },
+    });
+
+    this.notesTable.grant(putNote, "dynamodb:PutItem");
+
+    const api = new apigwv2.HttpApi(this, "api", {
+      corsPreflight: {
+        allowHeaders: ["Content-Type"],
+        allowMethods: [
+          apigwv2.CorsHttpMethod.GET,
+          apigwv2.CorsHttpMethod.OPTIONS,
+          apigwv2.CorsHttpMethod.POST,
+        ],
+        allowOrigins: ["*"],
+      },
+    });
+
+    const putNotesIntegration = new HttpLambdaIntegration(
+      "putNotesIntegration",
+      putNote
+    );
+
+    api.addRoutes({
+      path: "/notes",
+      methods: [apigwv2.HttpMethod.POST],
+      integration: putNotesIntegration,
+    });
+
+    new CfnOutput(this, "apiEndpoint", {
+      value: api.url!,
+    });
+  }
+}
